66ac0e2e6cc6b73297f3a8652a73674d6e9bcca6
Luke Firth <lukeantonfirth@gmail.com>
2019-06-22 00:30:03 +1000

    smoooth scroll

--- js/main.js
+++ js/main.js
@@ -68,6 +68,27 @@ $(document).ready(function() {
     }
   });
 
+  $(
+    'a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])'
+  ).click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - 160
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
   // $(window).scroll(function() {
   //   console.log("hello");
   //   // Add parallax scrolling to all images in .paralax-image container
